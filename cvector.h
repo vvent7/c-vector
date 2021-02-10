a5d68668cf336bb836d894bde305531b7d520435
Fernando Ventilari <fernandoventilari@gmail.com>
2021-02-10 01:17:06 -0400

    Comments formatted for better reading. Macros for Vector's pointers added: begin, end.

--- cvector.h
+++ cvector.h
@@ -12,61 +12,68 @@ typedef struct{
 extern size_t _vec_st;
 extern VectorHeader *_vec_hd;
 
-/*Verifying whether there's a MIN definition */
+/* Verifying whether there's a MIN definition */
 #ifndef MIN
-  #define MIN(x, y) (x<y ? x : y) /*Returns the minimum number (x or y). */
+  #define MIN(x, y) (x<y ? x : y) /* Returns the minimum number (x or y). */
 #endif
 
 #ifndef MAX
-  #define MAX(x, y) (x>y ? x : y) /*Returns the maximum number (x or y). */
+  #define MAX(x, y) (x>y ? x : y) /* Returns the maximum number (x or y). */
 #endif
 
-/*Macro for Vector's type (int, double, float, boolean, etc...) */
+/* Macro for Vector's type (int, double, float, boolean, etc...) */
 #define Vector(type) type**
 
-/*Verifies whether the Vector is valid */
+/* Verifies whether the Vector is valid */
 #define vector_is_valid(vec) (vec!=NULL && (*vec)!=NULL)
 
-/*Creates the vector and returns its address (pointer to array / pointer to pointer) */
-#define vector_create(type) ((Vector(type)) _vector_create(sizeof(type)))
-Vector(void) _vector_create(size_t tp_sz);
-
-/*Returns Vector's Header Address */
+/* Returns Vector's Header Address */
 #define vector_header(vec) (_vector_header((Vector(void)) vec))
 VectorHeader* _vector_header(Vector(void) vec);
 
-/*Returns Vector's Size */
+/*----------------------------------Constructor----------------------------------*/
+/* Creates the vector and returns its address 
+ * (pointer to array / pointer to pointer) */
+#define vector_create(type) ((Vector(type)) _vector_create(sizeof(type)))
+Vector(void) _vector_create(size_t tp_sz);
+/*==============================================================================*/
+
+/*-----------------------------------Pointers-----------------------------------*/
+/* Returns pointer to the begin of Vector */
+#define vector_begin(vec) ((*vec))
+
+/* Returns pointer to the end of Vector */
+#define vector_end(vec) ((*vec) + vector_header(vec)->sz)
+/*==============================================================================*/
+
+/*----------------------------------Dimensions----------------------------------*/
+/* Returns Vector's Size */
 #define vector_size(vec) (_vector_size((Vector(void)) vec))
 size_t _vector_size(Vector(void) vec);
 
-/*Returns Vector's Total Capacity */
+/* Returns Vector's Total Capacity */
 #define vector_capacity(vec) (_vector_capacity((Vector(void)) vec))
 size_t _vector_capacity(Vector(void) vec);
 
-/*Verifies whether the Vector is empty. */
+/* Verifies whether the Vector is empty */
 #define vector_empty(vec) (vector_size(vec)==0)
 
-/*Sets Vector's Size */
+/* Sets Vector's Size */
 #define vector_set_size(vec, size) (_vector_set_size((Vector(void)) vec, size))
 void* _vector_set_size(Vector(void) vec, size_t new_size);
 
-/*Sets Vector's Capacity */
+/* Sets Vector's Capacity */
 #define vector_set_capacity(vec, new_cap) (_vector_set_capacity((Vector(void)) vec,new_cap))
 void* _vector_set_capacity(Vector(void) vec, size_t new_cap);
+/*==============================================================================*/
 
-/*Clears Vector's content */
-#define vector_clear(vec) (vector_set_capacity(vec, 0))
-
-/*Frees all memory associated with the vector */
-#define vector_free(vec) (_vector_free((Vector(void)) vec))
-void _vector_free(Vector(void) vec);
-
-/*Sets a gap with specific length starting at specific index in the vector.
-  Returns: Pointer to begin of gap */
+/*-----------------------------------Modifiers-----------------------------------*/
+/* Sets a gap with specific length starting at specific index in the vector.
+ * Returns: Pointer to begin of gap */
 void* _vector_set_gap(Vector(void) vec, size_t index, size_t length);
 
-/*Inserts one single element inside vector at specific index
-  Returns: Pointer to element in the vector */
+/* Inserts one single element inside vector at specific index
+ * Returns: Pointer to element in the vector */
 #define vector_insert(vec, index, data)                              \
   (                                                                  \
     (                                                                \
@@ -79,7 +86,14 @@ void* _vector_set_gap(Vector(void) vec, size_t index, size_t length);
       : NULL                                                         \
   )
 
-/*Inserts data at Vector's end */
+/* Inserts data at Vector's end */
 #define vector_push_back(vec, data) (vector_insert(vec, vector_size(vec), data))
+
+/* Clears Vector's content */
+#define vector_clear(vec) (vector_set_capacity(vec, 0))
+
+/* Frees all memory associated with the vector */
+#define vector_free(vec) (_vector_free((Vector(void)) vec))
+void _vector_free(Vector(void) vec);
 
 #endif
